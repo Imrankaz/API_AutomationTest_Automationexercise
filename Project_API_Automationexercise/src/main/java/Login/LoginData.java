d1c5441d507dca980a743ff0a81dbb39f3ba14e2
Imrankaz <imran01.kaz@gmail.com>
2025-05-21 23:13:56 +0600

    LoginData updated

--- Project_API_Automationexercise/src/main/java/Login/LoginData.java
+++ Project_API_Automationexercise/src/main/java/Login/LoginData.java
@@ -10,7 +10,7 @@ public class LoginData {
     public static final String Invalid_password_signUpPart1Done = "12345678910";
     public static final String Valid_email = "bob25@gmail.com";
     public static final String Valid_password = "123456789123456789";
-    public static final String Valid_email2 = "bob25@gmail.com";
+    public static final String Valid_email2 = "bob26@gmail.com";
     public static final String Valid_password2 = "123456789123456789";
     public static final String UnRegistered_email = "bob20@gmail.com";  // so, have no password
     public static final String OldRegistered_email = "bob21@gmail.com"; //expired?  Valid_password = "123456789123456789";
